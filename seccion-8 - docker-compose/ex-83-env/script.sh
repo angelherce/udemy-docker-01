1cbd2141f3021823098224e0a9e0921e5cf405d5
angel.herce <angel.herce@imatia.com>
2022-08-09 21:00:33 +0200

    Seccion 8 - Docker Compose [83] - Se ha creado un fichero docker-compose.yml con el servicio de MySQL con algunas variables de entorno

---
+++ seccion-8 - docker-compose/ex-83-env/script.sh
@@ -0,0 +1,7 @@
+docker-compose -f ./docker-compose.v1.yml up -d
+docker-compose -f ./docker-compose.v2.yml up -d
+docker-compose -f ./docker-compose.v3.yml up -d
+
+# docker-compose -f ./docker-compose.v1.yml down
+# docker-compose -f ./docker-compose.v2.yml down
+# docker-compose -f ./docker-compose.v3.yml down
