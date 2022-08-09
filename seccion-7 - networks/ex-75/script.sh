58c71e30887a4f2715d74b5e5de88d83dd82bfa6
angel.herce <angel.herce@imatia.com>
2022-08-09 17:30:27 +0200

    Seccion 7 - Docker Networks [75] - Se ha creado un comando para crear un contenedor con una determinada IP en una Network

---
+++ seccion-7 - networks/ex-75/script.sh
@@ -0,0 +1,13 @@
+docker network create \
+    --driver bridge \
+    --subnet 172.128.10.0/24 \
+    --gateway 172.128.10.1 \
+    test-network
+
+docker run -dti \
+    --network test-network \
+    --ip 172.128.10.50 \
+    --name nginx \
+    centos:latest
+
+# docker network inspect test-network
