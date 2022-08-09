46bbe3c779792c26efa374df7364c60f9ed999de
angel.herce <angel.herce@imatia.com>
2022-08-09 17:02:43 +0200

    Seccion 7 - Dcoker Networks [71, 72] - Se ha creado el comando para crea una red y dos contenedores conectados a dicha red.

---
+++ seccion-7 - networks/ex-71-72/script.sh
@@ -0,0 +1,22 @@
+docker network create \
+    --driver bridge \
+    --subnet 172.124.10.0/24 \
+    --gateway 172.124.10.1 \
+    test-network-1
+
+docker run -dti \
+    --network test-network-1 \
+    --name test1 \
+    centos:latest
+
+docker run -dti \
+    --network test-network-1 \
+    --name test2 \
+    centos:latest
+
+# docker inspect test2 | grep IPAddress
+# docker exec test1 bash -c "ping 172.124.10.3"
+# docker inspect test1 | grep IPAddress
+# docker exec test2 bash -c "ping 172.124.10.2"
+# docker exec test1 bash -c "ping test2"
+# docker exec test2 bash -c "ping test1"
