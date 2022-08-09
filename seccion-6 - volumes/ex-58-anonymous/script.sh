d106c8d8096a7f117cb982c61b4578dec14ebfec
angel.herce <angel.herce@imatia.com>
2022-08-09 10:21:20 +0200

    Seccion 6 - Docker Volumes [58] - Se ha creado un comando para crear un contenedor de MySQL con un volumen anónimo

---
+++ seccion-6 - volumes/ex-58-anonymous/script.sh
@@ -0,0 +1,13 @@
+docker run \
+    -d \
+    -p3306:3306 \
+    -v /var/lib/mysql \
+    --name mysql \
+    -e "MYSQL_ROOT_PASSWORD=123456" \
+    -e "MYSQL_DATABASE=docker-db" \
+    -e "MYSQL_USER=docker-user" \
+    -e "MYSQL_PASSWORD=123456" \
+    mysql:5.7
+
+# mysqldump -u root -h 127.0.0.1 -p123456 sys > ./sys.dump.sql
+# mysql -u root -h 127.0.0.1 -p123456 docker-db < ./sys.dump.sql
