0130b82366a2dac6e48aa306de8eaf9d10a7600f
angel.herce <angel.herce@imatia.com>
2022-08-09 21:07:23 +0200

    Seccion 8 - Docker Compose [84] - Se ha creado un fichero docker-compose con el servicio de Nginx con un volumen de host y un volumen nombrado

---
+++ seccion-8 - docker-compose/ex-84-volumes/script.sh
@@ -0,0 +1,5 @@
+docker-compose -f ./docker-compose.named.yml up -d
+docker-compose -f ./docker-compose.host.yml up -d
+
+# docker-compose -f ./docker-compose.named.yml down -v
+# docker-compose -f ./docker-compose.host.yml down -v
